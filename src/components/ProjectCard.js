40454fb262fa8c2b2ae1484cd196d6c14caf9aa6
Eric Romero <eric.romero96@gmail.com>
2025-04-15 21:00:48 -0700

    Made adjustments so mobile looks cleaner.

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,7 +2,7 @@ import { Col } from "react-bootstrap"
 
 export const ProjectCard = ({ title, description, imgUrl, onImageClick }) => {
     return (
-        <Col sm={6} md={4}>
+        <Col sm={6} md={4} className="d-flex justify-content-center">
             <div className="proj-imgbox" onClick={() => onImageClick(imgUrl)} style={{cursor: "pointer"}}>
                 <img src={imgUrl} alt="projImg"/>
                 <div className="proj-text">
